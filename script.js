50ae0f604097eb43e3b85217e258c5b71b7fe6de
Justin Riley <20hz20khz@gmail.com>
2017-12-08 12:34:26 -0500

    init

---
+++ script.js
@@ -0,0 +1,15 @@
+let img1;
+
+function preload(){
+  img1 = loadImage('https://cdn.glitch.com/e3090096-8c2e-4fe1-b543-08d827467d32%2FSnowflake_(NL_icon).png?1512754425557');
+}
+
+function setup(){
+  createCanvas(windowWidth,windowHeight);
+  
+}
+
+function draw(){
+  background(0);//black
+  
+}
